a3067ac8031ca3abaadfbb3da204446f877843ae
LimJongHyun <limjh1358@gmail.com>
2024-04-19 14:48:49 +0900

    feat: Rename remove_odd -> split_odd_even
    홀수를 제거하는 것이 아닌 홀수와 짝수를 각각의 리스트에 담아 튜플로
    반환하도록 변경하였습니다.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def check_available():
         return 'It works!'
 
-def remove_odd(nums=[1,2,3,4]):
-        return [num for num in nums if num%2==0]
+def split_odd(nums=[1,2,3,4]):
+    odds=[]
+    evens=[]
+    for i in nums:
+        if i % 2 == 0:
+            evens.append(i)
+        else:
+            odds.append(i)
+    return (odds, evens)
+
 
 if __name__=='__main__':
         # print(check_available())
-        print(remove_odd([1,2,3,4,5,6,7,8,9,1,2,3,4,56,7,8,9,84,95,6,1]))
+        print(split_odd([1,2,3,4,5,6,7,8,9,1,2,3,4,56,7,8,9,84,95,6,1]))
