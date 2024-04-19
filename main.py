a55c5aa927d70d3bb983a5f03a9c2b499ae7184f
LimJongHyun <limjh1358@gmail.com>
2024-04-19 14:39:07 +0900

    feat: Add Feature remove_odd
    Using list comprehension, remove all odds from list.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def check_available():
         return 'It works!'
 
+def remove_odd(nums=[1,2,3,4]):
+        return [num for num in nums if num%2==0]
+
 if __name__=='__main__':
-        print(check_available())
+        # print(check_available())
+        print(remove_odd([1,2,3,4,5,6,7,8,9,1,2,3,4,56,7,8,9,84,95,6,1]))
